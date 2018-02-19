1a00ed474e8daf3b90bd024626c7e01d7f7fd6c9
joe arnold <joearnold@joes-MacBook-Air.local>
2018-02-18 20:38:18 -0700

    hw

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var Letter = require("./letter.js");
 var inquirer = require("inquirer");
+var colors = require("colors");
+
 var game = new Letter();
 var guesses = 0;
 var hangman = [
@@ -16,24 +18,35 @@ var hangman = [
 var inputArray = [];
 game.letterRender();
 var count = 0;
-var GameOn = function () {
+var GameOn = function() {
 
     inquirer.prompt([
         {
             type: "input",
             name: "userInput",
-            message: "Guess a letter!"
+            message: "Guess a letter!".america
         }
 
-
     ]).then(function (answer) {
-        var correct = false;
+        var correct = true;
         var lowerCase = game.wordArray.join("").toLowerCase().split("");
+        function checkArray() {
+            for (var i = 0; i < inputArray.length; i++) {
+                if (answer.userInput == inputArray[i]) {
+                    console.log("pick another letter!");
+                }
+            }
+        }
+        checkArray();
 
         if (lowerCase.indexOf(answer.userInput.toLowerCase()) !== -1) {
             correct = true;
-            console.log("\x1b[32m", "CORRECT!!")
+            console.log("CORRECT!!".green);
+            inputArray.push(answer.userInput);
+        } else if (answer.userInput == inputArray[i]) {
+            console.log("pick another one");
         }
+
         for (var i = 0; i < game.wordArray.length; i++) {
             if (answer.userInput.toLowerCase() == lowerCase[i]) {
                 game.underscoreArray[i] = game.wordArray[i];
@@ -41,40 +54,55 @@ var GameOn = function () {
             }
         }
 
-        if (gameWord.toLowerCase().indexOf(answer.userInput.toLowerCase()) == -1 && guesses < 9) {
+        if (gameWord.toLowerCase().indexOf(answer.userInput.toLowerCase()) == -1 && guesses < 8) {
 
             guesses++;
             console.log(hangman[guesses]);
+            correct = false;
+            inputArray.push(answer.userInput);
 
-            // console.log("\x1b[31m", "WRONG!!!")
-        } else if (guesses >= 9) { stopGame() }
+        } else if (guesses >= 8) { stopGame() }
         console.log("\x1b[37m" + game.underscoreString);
         GameOn();
 
         if (game.underscoreString.indexOf("_") == -1) {
-            console.log("you win!!")
+            console.log("You win!!".green);
             stopGame();
         }
+
     });
+        if (guesses == 8) {
+            console.log("You Lost".red);
+            stopGame();
+        }
 
 }
+
 function stopGame() {
     inquirer.prompt([
         {
             type: "list",
             name: "what",
             message: "Do you wanna play it again??",
-            choices: ["Yeap!", "Meh"]
+            choices: ["Yep!", "Nope"]
         }
     ]).then(function (answer) {
-        if (answer.what === "Yeap!") {
+        if (answer.what === "Yep!") {
             GameOn();
-            //need setTime stuff here - i think
+            
+            emitter.setMaxListeners(0);
+           
+
         } else {
+            console.log("See You Later!!".blue)
             process.exit()
         }
     })
 }
 GameOn();
+
+
+
+
 
 
--- word.js
+++ word.js
@@ -1,12 +1,12 @@
 // users list of words to guess from
 
-var wordList =  ['Jerry Seinfeld' , 'Elaine Benes' , 'George Costanza' , 'Newman' , 'Susan Ross' , 'Frank Costanza' , 'J Peterman' , 'Kenny Ban' , 'Uncle Leo' , 'Crazy Joe Davola']
+var wordList =  ['Jerry Seinfeld' , 'Elaine Benes' , 'George Costanza' , 'Newman' , 'Susan Ross' , 'Frank Costanza' , 'J Peterman' , 'Kenny Bania' , 'Uncle Leo' , 'Crazy Joe Davola']
 ;
 
 var word = function() {
     this.gameWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-    console.log(this.gameWord);
+    // console.log(this.gameWord);
 
 }
 
